434efec1a50b89a24a941dcf8b8629a0ff624da5
Soham Nagawanshi <ndsoham@tamu.edu>
2026-05-29 17:53:36 -0500

    Added argument parsing

--- main.py
+++ main.py
@@ -3,9 +3,29 @@ from strategy.signals import compute_signals
 from backtest.engine import run_backtest
 from metrics.performance import compute_metrics
 from dashboard.app import plot_dashboard
+import argparse
 
-aapl_data = fetch_ohlcv("aapl", "2021-11-9", "2026-05-28")
-signals = compute_signals(aapl_data)
-backtest = run_backtest(signals)
-metrics = compute_metrics(backtest)
-plot_dashboard(backtest, metrics, "AAPL")
+def parse_args():
+    
+    parser = argparse.ArgumentParser(description="MA Crossover Backtest")
+    
+    parser.add_argument("--ticker", type=str, default="AAPL")
+    parser.add_argument("--start", type=str, default="2021-11-9")
+    parser.add_argument("--end", type=str, default="2026-05-28")
+    parser.add_argument("--initial_capital", type=float, default=100000.0)
+    parser.add_argument("--short_window", type=int, default=50)
+    parser.add_argument("--long_window", type=int, default=200)
+    
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    
+    stock_data = fetch_ohlcv(args.ticker, args.start, args.end)
+    signals = compute_signals(stock_data, args.short_window, args.long_window)
+    backtest = run_backtest(signals, args.initial_capital)
+    metrics = compute_metrics(backtest)
+    plot_dashboard(backtest, metrics, args.ticker)
+
+if __name__ == "__main__":
+    main()
